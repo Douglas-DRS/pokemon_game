f8e75abdaf0fefa4f0606cbf76b39343d1e6a600
Douglas-DRS <contato.douglasrafael@proton.me>
2023-11-14 11:41:07 -0300

    :construction: first class added

---
+++ pokemon.py
@@ -0,0 +1,16 @@
+class Pokemon:
+
+	def __init__(self, tipo, especie):
+		self.tipo = tipo
+		self.especie = especie
+
+
+
+
+meu_pokemon = Pokemon('fogo', 'charmander')
+
+pokemon_amigo = Pokemon('eletrico', 'pikachu')
+
+print(meu_pokemon.tipo, meu_pokemon.especie)
+
+print(pokemon_amigo.tipo, pokemon_amigo.especie)
